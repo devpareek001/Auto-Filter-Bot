c8645d2be7f12e053d8ae0c9a5ef4170b53dc0c3
Dev_botz <deveshpareek001@gmail.com>
2025-08-05 11:22:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -12,6 +12,7 @@ from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidD
 from Script import script
 import pyrogram
 from info import *
+from helpers.allowlist import is_group_allowed
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto, WebAppInfo
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
@@ -2218,3 +2219,8 @@ async def advantage_spell_chok(client, message):
         await message.delete()
     except:
         pass
+
+@Bot.on_message(filters.command("id") & filters.group)
+async def group_id(client, message):
+    await message.reply_text(f"👥 This group's ID is:\n`{message.chat.id}`")
+	
