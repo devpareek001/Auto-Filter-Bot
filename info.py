1b6f60f7366a436797eb281886194fcf594e85ea
Dev_botz <deveshpareek001@gmail.com>
2025-09-11 23:32:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ AUTH_REQ_CHANNEL = [int(ch) for ch in AUTH_REQ_CHANNEL.strip().split()] if AUTH_
 #===========================================================
 #                shortner info 
 #===========================================================
-IS_VERIFY = is_enabled('IS_VERIFY', False)
+IS_VERIFY = is_enabled('IS_VERIFY', True)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002679851383'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002679851383'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
