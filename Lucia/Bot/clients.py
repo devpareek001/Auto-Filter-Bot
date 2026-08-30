6ab83f90b94d37fc34304a7b5b686121f94a698a
Dev_botz <deveshpareek001@gmail.com>
2026-08-30 11:27:58 +0530

    DevBot

--- Lucia/Bot/clients.py
+++ Lucia/Bot/clients.py
@@ -2,12 +2,12 @@ import asyncio
 from info import *
 from pyrogram import Client
 from Lucia.util.config_parser import TokenParser
-from . import multi_clients, work_loads, SilentX
+from . import multi_clients, work_loads, Dev
 from logging_helper import LOGGER
 
 
 async def initialize_clients():
-    multi_clients[0] = SilentX
+    multi_clients[0] = Dev
     work_loads[0] = 0
     all_tokens = TokenParser().parse_from_env()
     if not all_tokens:
